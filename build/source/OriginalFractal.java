75f38d58627cf7b9350c1c094629253c4d4655e6
cindelint <cindylee776@gmail.com>
2019-02-10 19:25:32 -0800

    added coloring, zooming, but zooming not work

--- build/source/OriginalFractal.java
+++ build/source/OriginalFractal.java
@@ -17,41 +17,25 @@ public class OriginalFractal extends PApplet {
 int mag;
 
 public void setup() {
-  mag = 300;
-  background(100);
   
-  translate(width/2, height/2);
-  plotSet(mag);
-  fill(0);
-  stroke(10);
-  line(-width/2,0,width/2,0);
-  line(0,-height/2,0,height/2);
+  mag = 300;
 }
 
 public void draw() {
-  if (keyPressed) {
-    if (keyCode == 38) { //up arrow
-      mag+=10;
-    } else if (keyCode == 40) { //down arrow
-      mag-=10;
-    } else if (key == 'w') {
-      translate(0,20);
-      println("this worked");
-    } else if (key == 's') {
-      translate(0,-20);
-    } else if (key == 'a') {
-      translate(-20,0);
-    } else if (key == 'd') {
-      translate(20,0);
-    }
-    background(100);
-    translate(width/2, height/2);
-    plotSet(mag);
-  }
+  noLoop();
+  background(100);
+  translate(width/2, height/2);
+  plotSet(mag);
+  //fill(0);
+  //stroke(10);
+  //line(-width/2,0,width/2,0); //x axis
+  //line(0,-height/2,0,height/2); //y axis
 }
 
 public void keyPressed() {
-
+  println("pressed");
+  mag+=200;
+  redraw();
 }
 
 public double[] testComplex(int iterations, float num, float inum) {
@@ -70,19 +54,48 @@ public double[] testComplex(int iterations, float num, float inum) {
 
 public void plotSet(int magnitude) {
   double inf = Double.POSITIVE_INFINITY;
-  for (float x=-2; x<=0.5f; x+=0.004f) {
-    for (float y=-2; y<=2; y+=0.004f) {
-      double c = testComplex(80,x,y)[0];
-      double i = testComplex(80,x,y)[1];
-      if (!Double.isInfinite(c) && !Double.isNaN(c) && !Double.isInfinite(i) && !Double.isNaN(i)) {
-        fill(255);
-        noStroke();
-        ellipse((float) x * magnitude,(float) y * magnitude,1,1);
+  println(-width/(magnitude*2));
+  for (float x=-width/(magnitude*2); x<width/(magnitude*2); x+=width/(magnitude*2000)) {
+    //for (float x=-2; x<2; x+=0.002) {
+    for (float y=-height/(magnitude*2); y<height/(magnitude*2); y+=height/(magnitude*2000)) {
+      //for (float y=-2; y<2; y+=0.002) {
+      if (Math.sqrt(x*x+y*y)<=2) {
+        double c = testComplex(80,x,y)[0];
+        double i = testComplex(80,x,y)[1];
+        if (!Double.isInfinite(c) && !Double.isNaN(c) && !Double.isInfinite(i) && !Double.isNaN(i)) {
+          fill(0);
+          noStroke();
+          ellipse((float) x * magnitude,(float) y * magnitude, 1, 1);
+        } else {
+          int col = coloringNum(x,y);
+          fill(255-(col-6)*8, (col+15)*7,80+(col-10)*4);
+          noStroke();
+          ellipse((float) x * magnitude, (float) y * magnitude, 1, 1);
+        }
       }
     }
   }
 }
-  public void settings() {  size(1000,800); }
+
+
+public int coloringNum(float num, float inum) {
+  //return # of iterations needed to reach 2 in any direction
+  float c = num;
+  float i = inum;
+  int coloringIterations = 0;
+  while (Math.abs(c) <= 5 || Math.abs(i) <= 5) {
+    if (coloringIterations < 300) {
+      float oldC = c;
+      c = c * c - i * i + num;
+      i = 2 * oldC * i + inum;
+      coloringIterations++;
+    } else {
+      return 0;
+    }
+  }
+  return coloringIterations;
+}
+  public void settings() {  size(1200,800); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalFractal" };
     if (passedArgs != null) {
